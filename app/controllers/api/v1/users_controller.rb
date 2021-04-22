8f4aaae7a22cebab39b83c68cdd0cb3d9c3858f9
anpham148 <anpham148@gmail.com>
2021-04-22 15:42:04 +0100

    updated users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,28 @@
 class Api::V1::UsersController < ApplicationController
+    
+    def create
+        @user = User.new(user_params)
+        if user.save
+            render json: @user, status: :accepted
+        else 
+            render json: {errors: @user.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name)
+    end
+
 
 end
