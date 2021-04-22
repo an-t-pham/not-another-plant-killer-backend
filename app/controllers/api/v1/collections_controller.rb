4e354fc66b558337f22ccef62e603b9c2f5f035e
anpham148 <anpham148@gmail.com>
2021-04-22 15:17:14 +0100

    updated collection controller + route

--- app/controllers/api/v1/collections_controller.rb
+++ app/controllers/api/v1/collections_controller.rb
@@ -1,3 +1,27 @@
-class API::V1::CollectionsController < ApplicationController
+class Api::V1::CollectionsController < ApplicationController
 
+    def create
+        @collection = Collection.new(collection_params)
+        if @collection.save
+            render json: @plant, status: :accepted
+        else 
+            render json: {errors: @plant.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    def show
+        @collection = Collection.find(params[:id])
+        render json: @collection
+    end
+
+    def destroy
+        @collection = Collection.find(params[:id])
+        @collection.destroy
+    end
+
+
+    private
+    def collection_params
+        params.require(:collection).permit(:name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :collections
+      resources :collections, only: [:create, :show, :destroy]
       resources :plants, only: [:index, :show]
       resources :users
     end
