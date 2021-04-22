1e31844b501bba35f1d2b15968511247584767a4
An Pham <53116737+anpham148@users.noreply.github.com>
2021-04-22 14:49:28 +0100

    updated plant controller api (#2)

--- app/controllers/api/v1/plants_controller.rb
+++ app/controllers/api/v1/plants_controller.rb
@@ -1,4 +1,29 @@
 class Api::V1::PlantsController < ApplicationController
 
-   
+    def index
+        @plants = Plant.all
+        render json: @plants
+    end
+    
+    def create
+        @plant = Plant.new(plant_params)
+        if @plant.save
+            render json: @plant, status: :accepted
+        else 
+            render json: {errors: @plant.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    def show
+        @plant = Plant.find(params[:id])
+        render json: @plant
+    end
+
+
+    private
+    def plant_params
+        params.require(:plant).permit(:name, :aka, :description, :size_pot, :pet_friendly)
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :collections
-      resources :plants
+      resources :plants, only: [:index, :show]
       resources :users
     end
   end
