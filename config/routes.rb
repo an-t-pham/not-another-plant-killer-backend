c76c9b3969c8281b831b4d7f5c34bbcc4ec3a24a
anpham148 <anpham148@gmail.com>
2021-04-26 19:46:06 +0100

    edited user_plant in plant controller to user_plants

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :plants, only: [:index, :show, :create]
+      resources :plants, only: [:index, :show]
       resources :users, only: [:index, :create, :show, :destroy] do
-        get '/plants' => 'plants#user_plant'
+        get '/plants' => 'plants#user_plants'
+        resources :plants, only: [:create]
         resources :collections, only: [:index, :index_plant, :create, :show, :destroy] 
       end
         
