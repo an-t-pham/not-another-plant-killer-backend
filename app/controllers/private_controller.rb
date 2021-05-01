110cec25d53bdad49e89d71b7030c83e3818d1be
anpham148 <anpham148@gmail.com>
2021-05-01 13:57:57 +0100

    changes

--- app/controllers/private_controller.rb
+++ app/controllers/private_controller.rb
@@ -2,7 +2,7 @@ class PrivateController < ActionController::API
     include Secured
   
     def private
-      render json: 'Hello from a private endpoint! You need to be authenticated to see this.'
+      render json: { message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.' }
     end
   
     def private_scoped
