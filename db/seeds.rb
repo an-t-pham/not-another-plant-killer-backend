13d8382cf330a27e8ef7a64fb8d4eaa8f6e042da
anpham148 <anpham148@gmail.com>
2021-05-08 15:10:55 +0100

    added plant image_url for seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,8 @@ lady_palm = Plant.create(
     aka: "Rhapish Excelsa", 
     description: "Broadleaf lady palms are a strange species of fan palms that actually don’t exist in the wild — they came from Chinese cultivators.They’re exceptionally good indoor houseplants, because they clean toxins from the air. The main four they process are carbon dioxide, formaldehyde, xylene, and ammonia. They’re also easy to care for, making them good for gardening beginners!",
     size_pot: 10,
-    pet_friendly: true
+    pet_friendly: true,
+    image_url: "https://dh1muyqdu88ie.cloudfront.net/wp-content/uploads/2017/02/22122638/broadleaf-lady-palm-tree.jpg"
 )
 
 lady_palm.light = two
@@ -38,7 +39,8 @@ evergreen = Plant.create(
     aka: "Aglaonemas Modestum",
     description: "There are many hybrid varieties of the Chinese evergreen available which have been cultivated over the last century. This is because of their increasing popularity for indoor growers to use them as ornamental plants for room decoration.",
     size_pot: 6,
-    pet_friendly: false
+    pet_friendly: false,
+    image_url: "https://cdn.atwilltech.com/flowerdatabase/c/chinese-evergreen-plant-aglaonema-commutatum-PL00208.425.jpg"
 )
 
 evergreen.light = two
@@ -49,7 +51,8 @@ ivy = Plant.create(
     aka: "Hedera Helix",
     description: "To the ancient Greeks and Romans, glossy-leafed, dark green ivy was sacred to the god Dionysus (Bacchus in Rome). The pagan druids reflected on ivy in the Christmas carol, The Holly and the Ivy, where the plant represents female divinity. This plant clearly had an impact on ancient cultures but did not stop there. The evergreen vines of this invasive species have covered vast areas of Europe.",
     size_pot: 5,
-    pet_friendly: false
+    pet_friendly: false,
+    image_url: "https://thelittlebotanical.com/wp-content/uploads/2019/07/10_JT_010_1000x1000.jpg"
 )
 
 ivy.light = two
@@ -60,7 +63,8 @@ snakey = Plant.create(
     aka: "Sansevieria",
     description: "Snake plants are native to Africa and Madagascar so thrive in warmer conditions and love bright light but will be quite happy in partially shaded areas. They also don’t mind a little neglect so you can enjoy their presence without worrying about them needing too much care. It forms a bundle of erect pointy deep green leaves. When they flower they produce a single spike of small white flowers.",
     size_pot: 5,
-    pet_friendly: false
+    pet_friendly: false,
+    image_url: "https://res.cloudinary.com/patch-gardens/image/upload/c_fill,f_auto,h_840,q_auto:good,w_840/v1563806745/products/snake-plant-cc1865.jpg"
 )
 
 snakey.light = one
@@ -71,7 +75,8 @@ lily = Plant.create(
     aka: "Spathiphyllum",
     description: "Peace lilies, also known as closet plants, are a popular choice for offices and homes. When it comes to indoor plants, peace lily plants are some of the easiest to care for. But, while peace lily plant care is easy, proper growing conditions are still important. Let’s take a look at the care of peace lilies.",
     size_pot: 6,
-    pet_friendly: false
+    pet_friendly: false,
+    image_url: "https://www.mydomaine.com/thmb/9aB79USSkhiCVgGyXh1TBSvM7v8=/900x900/filters:fill(auto,1)/1566417254329_20190821-1566417255317-b9314f1d9f7a4668a466c5ffb1913a8f.jpg"
 )
 
 lily.light = one
@@ -82,7 +87,8 @@ orchid = Plant.create(
     aka: "Phalaenopsis",
     description: "Moth orchids grow well in centrally heated rooms and have long-lasting flowers produced all year round. They are one of the most popular indoor orchids.",
     size_pot: 5,
-    pet_friendly: true
+    pet_friendly: true,
+    image_url: "https://www.whiteflowerfarm.com/mas_assets/cache/image/6/7/d/a/26586.Jpg"
 )
 
 orchid.light = one
@@ -93,7 +99,8 @@ dracaena = Plant.create(
     aka: "Madagascar dragon tree",
     description: "Commonly known as red-edge dracaena or Madagascar dragon tree, Dracaena marginata is an evergreen tree that can grow eight to 15 feet high and three to eight feet wide with proper care. It has stiff purplish-red leaves and slim, curving stalks for trunks. Since it cannot tolerate low light but is not frost hardy, it is often grown indoors. They make excellent houseplants because they are drought tolerant and among the more forgiving dracaena plants.",
     size_pot: 21,
-    pet_friendly: false
+    pet_friendly: false,
+    image_url: "https://img.crocdn.co.uk/images/products2/pl/20/00/02/77/pl2000027736.jpg?width=940&height=940"
 )
 
 dracaena.light = three
@@ -104,7 +111,8 @@ fig = Plant.create(
     aka: "Ficus benjamina",
     description: "Ficus benjamina, commonly known as weeping fig, benjamin fig or ficus tree, and often sold in stores as just ficus, is a species of flowering plant in the family Moraceae, native to Asia and Australia. It is the official tree of Bangkok.",
     size_pot: 20,
-    pet_friendly: true
+    pet_friendly: true,
+    image_url: "https://images.bunches.co.uk/products/large/weeping-fig-tree-1.jpg"
 )
 
 fig.light = three
@@ -115,7 +123,8 @@ aloe = Plant.create(
     aka: "Aloe Vera",
     description: "Aloe vera is a succulent plant species of the genus Aloe. An evergreen perennial, it originates from the Arabian Peninsula, but grows wild in tropical, semi-tropical, and arid climates around the world. It is cultivated for agricultural and medicinal uses.",
     size_pot: 5,
-    pet_friendly: false
+    pet_friendly: false,
+    image_url: "https://www.ikea.com/gb/en/images/products/aloe-vera-potted-plant-aloe__0653978_pe708207_s5.jpg?f=s"
 )
 
 aloe.light = four
@@ -126,7 +135,8 @@ jasmine = Plant.create(
     aka: "Stephanotis floribunda",
     description: "the Madagascar jasmine, waxflower, Hawaiian wedding flower, or bridal wreath is a species of flowering plant in the family Apocynaceae, native to Madagascar.",
     size_pot: 15,
-    pet_friendly: false
+    pet_friendly: false,
+    image_url: "https://s3.amazonaws.com/finegardening.s3.tauntoncloud.com/app/uploads/2020/03/12160501/FG193-ig-fragrant-houseplants-1.jpg"
 )
 
 jasmine.light = four
@@ -137,7 +147,8 @@ violet = Plant.create(
     aka: "Saintpaulia",
     description: "Some indoor gardeners shy away from growing the frilly and elegant African violet (Saintpaulia) because they are intimidated by African violet care. African violet plants do have a few quirks, but learning about them and the proper care of African violets can make growing the plants less intimidating.",
     size_pot: 4,
-    pet_friendly: true
+    pet_friendly: true,
+    image_url: "https://africanvioletresourcecenter.com/wp-content/uploads/2020/09/unnamed-111.jpg"
 )
 
 violet.light = four
