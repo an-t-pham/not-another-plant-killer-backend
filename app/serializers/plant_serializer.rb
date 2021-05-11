343bf4be205a06848d7fc3c2ba7425c8c4918b5e
An Pham <53116737+anpham148@users.noreply.github.com>
2021-05-11 11:18:35 +0100

    added formatted name for plant model (#18)

--- app/serializers/plant_serializer.rb
+++ app/serializers/plant_serializer.rb
@@ -1,4 +1,8 @@
 class PlantSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :aka, :description, :size_pot, :pet_friendly, :water, :light, :image_url, :slug
+  attributes :name, :aka, :description, :size_pot, :pet_friendly, :water, :light, :image_url, :slug, :formatted_name
+
+  def formatted_name
+    object.name.capitalize
+  end
 end
