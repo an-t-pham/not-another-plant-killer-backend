bbde368418070ad11ef37d8af5875c34e2a170f4
anpham148 <anpham148@gmail.com>
2021-06-01 14:02:33 +0100

    deleted private controller + private route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  
-  get '/private' => 'private#private'
+
   namespace :api do
     namespace :v1 do
       resources :plants, only: [:index, :create, :show, :update, :destroy]
